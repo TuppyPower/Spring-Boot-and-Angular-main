e011a24b4b3346ea71b17b976d8b2eeddceca759
Paulo Tuppy <130563312+TuppyPower@users.noreply.github.com>
2023-11-20 20:24:04 -0300

    Add files via upload

--- superheroes/src/main/java/com/example/springbootsuperheroes/superheroes/antiHero/controller/AntiHeroController.java
+++ superheroes/src/main/java/com/example/springbootsuperheroes/superheroes/antiHero/controller/AntiHeroController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -16,13 +17,16 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+@CrossOrigin(allowedHeaders = "Content-type")
 @AllArgsConstructor
 @RestController
 @RequestMapping("api/v1/anti-heroes")
+@PreAuthorize("isAuthenticated()")
 public class AntiHeroController {
     private final AntiHeroService service;
     private final ModelMapper mapper;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public List<AntiHeroDto> getAntiHeroes(Pageable pageable) {
         int toSkip = pageable.getPageSize() * pageable.getPageNumber();
